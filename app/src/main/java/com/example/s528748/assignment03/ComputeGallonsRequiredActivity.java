478458e71bf96c93acd6ca987b7197ed59cf19ea
ShivaTej <shivateja219@gmail.com>
2017-09-20 16:16:46 -0500

    Added button backgrounds

--- app/src/main/java/com/example/s528748/assignment03/ComputeGallonsRequiredActivity.java
+++ app/src/main/java/com/example/s528748/assignment03/ComputeGallonsRequiredActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ComputeGallonsRequiredActivity extends AppCompatActivity {
 
@@ -14,16 +15,24 @@ public class ComputeGallonsRequiredActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compute_gallons_required);
     }
 
-    public void gallonsRequired(View v){
+    public void gallonsRequired(View v) {
         double gallonsReq;
         EditText distance = (EditText) findViewById(R.id.editText3);
         EditText mileage = (EditText) findViewById(R.id.editText2);
-        double dist =Double.parseDouble(distance.getText().toString());
-        double mil =Double.parseDouble(mileage.getText().toString());
-        gallonsReq = (dist / mil);
-        Intent intent = new Intent(this, PriceDisplayActivity.class);
-        intent.putExtra("gallonsrequired", gallonsReq);
-        startActivity(intent);
+        if (distance.length() > 0 && mileage.length() > 0) {
+            double dist = Double.parseDouble(distance.getText().toString());
+            double mil = Double.parseDouble(mileage.getText().toString());
+            gallonsReq = (dist / mil);
+            Intent intent = new Intent(this, PriceDisplayActivity.class);
+            intent.putExtra("gallonsrequired", gallonsReq);
+            startActivity(intent);
+        }
+        else if(distance.length() == 0 && mileage.length() == 0)
+            Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT).show();
+        else if(mileage.length() == 0)
+            Toast.makeText(this, "Enter mileage", Toast.LENGTH_SHORT).show();
+        else if(distance.length() == 0)
+            Toast.makeText(this, "Enter distance", Toast.LENGTH_SHORT).show();
     }
 
 
--- app/src/main/java/com/example/s528748/assignment03/GallonEntryActivity.java
+++ app/src/main/java/com/example/s528748/assignment03/GallonEntryActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class GallonEntryActivity extends AppCompatActivity {
 
@@ -18,13 +19,15 @@ public class GallonEntryActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String value = editText.getText().toString();
 
-        if(value!=null) {
+        if(value.length()>0) {
             double gallons = Double.parseDouble(value);
             Intent intent;
             intent = new Intent(this, PriceDisplayActivity.class);
             intent.putExtra("gallons", gallons);
             startActivity(intent);
         }
+        else
+            Toast.makeText(this, "invalid input", Toast.LENGTH_SHORT).show();
 
     }
 }
